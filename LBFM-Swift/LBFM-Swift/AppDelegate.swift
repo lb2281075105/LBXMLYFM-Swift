44a93dd8a0ef1893ef50b6e0a6db675ed1b180b8
liubo <2281075105@qq.com>
2019-02-01 11:26:13 +0800

    目录结构

--- LBFM-Swift/LBFM-Swift/AppDelegate.swift
+++ LBFM-Swift/LBFM-Swift/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import ESTabBarController_swift
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        
         return true
     }
 
---
+++ LBFM-Swift/LBFM-Swift/Controller/Find(发现)/LBFMFindController.swift
@@ -0,0 +1,30 @@
+//
+//  LBFMFindController.swift
+//  LBFM-Swift
+//
+//  Created by liubo on 2019/2/1.
+//  Copyright © 2019 刘博. All rights reserved.
+//
+
+import UIKit
+
+class LBFMFindController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
