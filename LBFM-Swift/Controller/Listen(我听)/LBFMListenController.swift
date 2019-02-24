8058cb02c68d4671ddbf8d368a16be7a20be0005
liubo <2281075105@qq.com>
2019-02-24 14:07:27 +0800

    添加头部

--- LBFM-Swift/Controller/Listen(我听)/LBFMListenController.swift
+++ LBFM-Swift/Controller/Listen(我听)/LBFMListenController.swift
@@ -7,25 +7,111 @@
 //
 
 import UIKit
+import LTScrollView
 
 class LBFMListenController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        view.backgroundColor = UIColor.white
-
+    // 头部 - headerView
+    private lazy var headerView:LBFMListenHeaderView = {
+        let view = LBFMListenHeaderView.init(frame: CGRect(x:0, y:0, width:LBFMScreenWidth, height:120))
+        return view
+    }()
+    
+    // 添加控制器
+    private lazy var viewControllers: [UIViewController] = {
+        let listenSubscibeVC = LBFMListenSubscibeController()
+        let listenChannelVC = LBFMListenChannelController()
+        let listenRecommendVC = LBFMListenRecommendController()
+        return [listenSubscibeVC, listenChannelVC, listenRecommendVC]
+    }()
+    
+    // 添加l控制器上部标题
+    private lazy var titles: [String] = {
+        return ["订阅", "一键听", "推荐"]
+    }()
+    
+    
+    // 导航栏左边按钮
+    private lazy var leftBarButton:UIButton = {
+        let button = UIButton.init(type: UIButtonType.custom)
+        button.frame = CGRect(x:0, y:0, width:30, height: 30)
+        button.setImage(UIImage(named: "msg"), for: UIControlState.normal)
+        button.addTarget(self, action: #selector(leftBarButtonClick), for: UIControlEvents.touchUpInside)
+        return button
+    }()
+    // 导航栏右边按钮
+    private lazy var rightBarButton:UIButton = {
+        let button = UIButton.init(type: UIButtonType.custom)
+        button.frame = CGRect(x:0, y:0, width:30, height: 30)
+        button.setImage(UIImage(named: "搜索"), for: UIControlState.normal)
+        button.addTarget(self, action: #selector(rightBarButtonClick), for: UIControlEvents.touchUpInside)
+        return button
+    }()
+    // 导航栏左边消息点击事件
+    @objc func leftBarButtonClick() {
+        print("左边按钮")
+    }
+    // 导航栏左边消息点击事件
+    @objc func rightBarButtonClick() {
+        print("右边按钮")
     }
     
+    private lazy var layout: LTLayout = {
+        let layout = LTLayout()
+        layout.isAverage = true
+        layout.sliderWidth = 80
+        layout.titleViewBgColor = UIColor.white
+        layout.titleColor = UIColor(r: 178, g: 178, b: 178)
+        layout.titleSelectColor = UIColor(r: 16, g: 16, b: 16)
+        layout.bottomLineColor = UIColor.red
+        layout.sliderHeight = 56
+        /* 更多属性设置请参考 LTLayout 中 public 属性说明 */
+        return layout
+    }()
+    
+    private lazy var advancedManager: LTAdvancedManager = {
+        let statusBarH = UIApplication.shared.statusBarFrame.size.height
+        let advancedManager = LTAdvancedManager(frame: CGRect(x: 0, y: LBFMNavBarHeight, width: LBFMScreenWidth, height: LBFMScreenHeight - LBFMNavBarHeight), viewControllers: viewControllers, titles: titles, currentViewController: self, layout: layout, headerViewHandle: {[weak self] in
+            guard let strongSelf = self else { return UIView() }
+            let headerView = strongSelf.headerView
+            return headerView
+        })
+        /* 设置代理 监听滚动 */
+        advancedManager.delegate = self
+        /* 设置悬停位置 */
+        // advancedManager.hoverY = - LBFMNavBarHeight
+        /* 点击切换滚动过程动画 */
+        // advancedManager.isClickScrollAnimation = true
+        /* 代码设置滚动到第几个位置 */
+        // advancedManager.scrollToIndex(index: viewControllers.count - 1)
+        return advancedManager
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // 导航栏左右item
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: leftBarButton)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(customView: rightBarButton)
+        self.automaticallyAdjustsScrollViewInsets = false
+        view.addSubview(advancedManager)
+        advancedManagerConfig()
     }
-    */
 
+    deinit {
+        print(" --- deinit --- ")
+    }
+}
+extension LBFMListenController : LTAdvancedScrollViewDelegate {
+    // 具体使用请参考以下
+    private func advancedManagerConfig() {
+        // 选中事件
+        advancedManager.advancedDidSelectIndexHandle = {
+            print("选中了 -> \($0)")
+        }
+    }
+    
+    func glt_scrollViewOffsetY(_ offsetY: CGFloat) {
+        
+    }
 }
---
+++ LBFM-Swift/Controller/Listen(我听)/View/LBFMListenHeaderView.swift
@@ -0,0 +1,76 @@
+//
+//  LBFMListenHeaderView.swift
+//  LBFM-Swift
+//
+//  Created by liubo on 2019/2/24.
+//  Copyright © 2019 刘博. All rights reserved.
+//
+
+import UIKit
+
+class LBFMListenHeaderView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.white
+        setUpLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // 布局
+    func setUpLayout()  {
+        
+        let downView = UIView()
+        downView.backgroundColor = LBFMDownColor
+        self.addSubview(downView)
+        downView.snp.makeConstraints { (make) in
+            make.left.right.bottom.equalToSuperview()
+            make.height.equalTo(10)
+        }
+        
+        
+        let margin:CGFloat = self.frame.width/8
+        let titleArray = ["下载","历史","已购","喜欢"]
+        let imageArray = ["下载","历史","购物车","喜欢"]
+        let numArray = ["暂无","8","暂无","25"]
+        for index in 0..<4 {
+            let button = UIButton.init(frame: CGRect(x:margin*CGFloat(index)*2+margin/2,y:10,width:margin,height:margin))
+            button.setImage(UIImage(named: imageArray[index]), for: UIControlState.normal)
+            self.addSubview(button)
+            
+            let titleLabel = UILabel()
+            titleLabel.textAlignment = .center
+            titleLabel.text = titleArray[index]
+            titleLabel.font = UIFont.systemFont(ofSize: 15)
+            titleLabel.textColor = UIColor.gray
+            self.addSubview(titleLabel)
+            titleLabel.snp.makeConstraints({ (make) in
+                make.centerX.equalTo(button)
+                make.width.equalTo(margin+20)
+                make.top.equalTo(margin+10)
+            })
+            
+            let numLabel = UILabel()
+            numLabel.textAlignment = .center
+            numLabel.text = numArray[index]
+            numLabel.font = UIFont.systemFont(ofSize: 14)
+            numLabel.textColor = UIColor.gray
+            self.addSubview(numLabel)
+            numLabel.snp.makeConstraints({ (make) in
+                make.centerX.equalTo(button)
+                make.width.equalTo(margin+20)
+                make.top.equalTo(margin+10+25)
+            })
+            button.tag = index
+            button.addTarget(self, action: #selector(gridBtnClick(sender:)), for: UIControlEvents.touchUpInside)
+            
+        }
+
+    }
+    @objc func gridBtnClick(sender:UIButton){
+        print("点击")
+    }
+}
