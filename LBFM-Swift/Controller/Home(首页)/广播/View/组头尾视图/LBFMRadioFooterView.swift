aa0d69167783f6ad6703b4295d23f426f5340ad9
liubo <2281075105@qq.com>
2019-02-27 09:51:03 +0800

    添加组头尾视图

---
+++ LBFM-Swift/Controller/Home(首页)/广播/View/组头尾视图/LBFMRadioFooterView.swift
@@ -0,0 +1,20 @@
+//
+//  LBFMRadioFooterView.swift
+//  LBFM-Swift
+//
+//  Created by liubo on 2019/2/27.
+//  Copyright © 2019 刘博. All rights reserved.
+//
+
+import UIKit
+
+class LBFMRadioFooterView: UICollectionReusableView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = LBFMButtonColor
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
