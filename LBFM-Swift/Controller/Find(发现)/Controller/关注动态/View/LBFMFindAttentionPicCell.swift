b048e55378417681778ad6bb92afe20c96d16f02
liubo <2281075105@qq.com>
2019-02-28 10:40:58 +0800

    提交修改

--- LBFM-Swift/Controller/Find(发现)/Controller/关注动态/View/LBFMFindAttentionPicCell.swift
+++ LBFM-Swift/Controller/Find(发现)/Controller/关注动态/View/LBFMFindAttentionPicCell.swift
@@ -18,10 +18,10 @@ class LBFMFindAttentionPicCell: UICollectionViewCell {
         self.addSubview(self.imageView)
         self.imageView.layer.masksToBounds = true
         self.imageView.layer.cornerRadius = 5
-        self.imageView.contentMode = UIView.ContentMode.scaleToFill
+        self.imageView.contentMode = UIView.ContentMode.scaleAspectFill
         self.imageView.clipsToBounds = true
         self.imageView.snp.makeConstraints { (make) in
-            make.left.top.right.bottom.equalToSuperview()
+            make.edges.equalToSuperview()
         }
     }
     
